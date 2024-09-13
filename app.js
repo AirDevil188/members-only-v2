fb6c1698b501b263d7ceae1cf19ebc5fdb578dc1
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-09-13 15:08:46 +0200

    added public dir

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const port = process.env.PORT;
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   session({
