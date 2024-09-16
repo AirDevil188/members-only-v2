05dadcf307875cf687e4ecc6a1bdc4723f6f98b5
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-09-16 21:27:51 +0200

    added getSignUp and postSignUp middlewares in signUpController and added signUpRouter with get and post route

---
+++ controllers/signUpController.js
@@ -0,0 +1,64 @@
+const asyncHandler = require("express-async-handler");
+const db = require("../db/queries");
+const bcrypt = require("bcryptjs");
+const { validationResult, body } = require("express-validator");
+
+const lengthErr = "must contain at least one character";
+
+const validateUser = [
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage(`First Name ${lengthErr}`),
+  body("last_name")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage(`Last Name ${lengthErr}`),
+  body("username")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage(`Username ${lengthErr}`),
+  body("password")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage(`Password ${lengthErr}`),
+];
+
+const getSignUp = asyncHandler(async (req, res, next) => {
+  res.render("sign-up", {
+    title: "Sign Up",
+  });
+});
+
+const postSignUp = [
+  validateUser,
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render("sign-up", {
+        title: "Sign Up",
+      });
+    }
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+      try {
+        if (err) {
+          return next(err);
+        }
+        await db.createNewUser(
+          req.body.first_name,
+          req.body.last_name,
+          req.body.username,
+          hashedPassword
+        );
+        res.redirect("/");
+      } catch (err) {
+        return next(err);
+      }
+    });
+  }),
+];
+
+module.exports = {
+  getSignUp,
+  postSignUp,
+};
