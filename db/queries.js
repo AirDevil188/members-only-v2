0fd2756d238ea51cae941a336062d2ef3c4337b4
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-09-16 22:17:50 +0200

    added deserializeUser query

--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,7 @@
 const pool = require("./pool");
 
 async function searchForUser(username) {
-  const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
+  const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [
     username,
   ]);
   return rows[0];
@@ -14,7 +14,13 @@ async function createNewUser(first_name, last_name, username, password) {
   );
 }
 
+async function deserializeUser(id) {
+  const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+  return rows[0];
+}
+
 module.exports = {
   searchForUser,
   createNewUser,
+  deserializeUser,
 };
