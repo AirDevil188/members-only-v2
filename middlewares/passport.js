769e65bf0c1a7436d8a90c24b4f1fbbb2c78ee37
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-09-16 22:19:27 +0200

    returned done with user in localPassportStrategy, added deserialize and serialize functions

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -17,6 +17,7 @@ const localPassportStrategy = () => {
         if (!match) {
           return done(null, false, { message: "Incorrect password" });
         }
+        return done(null, user);
       } catch (err) {
         return done(err);
       }
@@ -25,3 +26,16 @@ const localPassportStrategy = () => {
 };
 
 localPassportStrategy();
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.deserializeUser(id);
+    done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+});
