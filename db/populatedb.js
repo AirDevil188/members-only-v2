2eb2f8928b6ba630385e3ea965ff8f504552efed
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-09-16 15:03:53 +0200

    written queries for creating tables

---
+++ db/populatedb.js
@@ -0,0 +1,20 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
+    first_name VARCHAR(255),
+    last_name VARCHAR (255),
+    full_name TEXT GENERATED ALWAYS AS (first_name || last_name) STORED, 
+    username VARCHAR(255),
+    password VARCHAR(255));
+
+CREATE TABLE IF NOT EXISTS messages(
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
+    title TEXT, 
+    text TEXT, 
+    timestamp TIMESTAMP, 
+    username  INTEGER REFERENCES users(id));
+`;
