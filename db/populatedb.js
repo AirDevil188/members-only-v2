a69960629847cf374f622e70e6841619cbdfd6ca
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-09-19 10:58:01 +0200

    added admin column with default value of false

--- db/populatedb.js
+++ db/populatedb.js
@@ -10,7 +10,8 @@ CREATE TABLE IF NOT EXISTS users(
     full_name TEXT GENERATED ALWAYS AS (first_name || ' ' || last_name) STORED, 
     username VARCHAR(255),
     password VARCHAR(255),
-    member  BOOLEAN DEFAULT FALSE);
+    member  BOOLEAN DEFAULT FALSE,
+    admin   BOOLEAN DEFAULT FALSE);
 
 CREATE TABLE IF NOT EXISTS messages(
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
